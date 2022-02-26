5433338a3cce27cfadb6246d0158a4369cb4536f
eric-michael <ericmichael8192@gmail.com>
2022-02-26 10:20:24 -0500

    Replace standard nav bar with MUI AppBar

--- client/src/components/layout/MainNav.js
+++ client/src/components/layout/MainNav.js
@@ -6,33 +6,41 @@ import classes from "./MainNav.module.css";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function MainNav() {
+  const theme = createTheme({
+    palette: {
+      c_primary: {
+        main: "#455a64",
+        contrastText: "#fff",
+      },
+      c_button: {
+        main: "#fff",
+      },
+    },
+  });
   return (
     <React.Fragment>
-      <header className={classes.header}>
-        <div>MainNav</div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">View All</Link>
-            </li>
-            <li>
-              <Link to="/favorites">Favorites</Link>
-            </li>
-            <li>
-              <Link to="/new">New Entry</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar><Link to="/"><Typography>Home</Typography></Link></Toolbar>
-        </AppBar>
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" color="c_primary">
+            <Toolbar>
+              <Button component={Link} to="/" variant="text" color="c_button">
+                View All
+              </Button>
+              <Button component={Link} to="/favorites" variant="text" color="c_button">
+                Favorites
+              </Button>
+              <Button component={Link} to="/new" variant="text" color="c_button">
+                New Entry
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
